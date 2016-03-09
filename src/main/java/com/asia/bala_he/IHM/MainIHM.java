cd97d252a351e9663b21bd4a0ff52180ecd653bc
balastephane <stephane_bala@yahoo.fr>
2016-03-09 20:50:58 +0100

    Design de la grille

--- src/main/java/com/asia/bala_he/IHM/MainIHM.java
+++ src/main/java/com/asia/bala_he/IHM/MainIHM.java
@@ -20,14 +20,52 @@ public class MainIHM {
 		displayBoard(bm.getBoard());
 	}
 	
+	//Affichage de la grille
 	public static void displayBoard(int[][] board){
 		for(int i=0; i<board.length;i++){
 			for(int j=0;j<board[0].length;j++){
-				System.out.print("["+board[i][j]+"]");
+				
+				/*Bord en bas à gauche*/
+				if(i==board.length-1 && j==0)
+				{
+					System.out.print("|_ ");
+				}
+				
+				/*Bord en bas à gauche*/
+				else if(i==board.length-1 && j==board[0].length-1)
+				{
+					System.out.print(" _|");
+				}
+				
+				/*Bord de gauche*/
+				else if(j==0)
+				{
+					System.out.print("|  ");
+				}
+				
+				/*Bord de droite*/
+				else if(j == board[0].length-1)
+				{
+					System.out.print("  |");
+				}
+				
+				/*Bord du bas*/
+				else if(i==board.length-1 && (j!=0 && j!=board[0].length-1 ))
+				{
+					System.out.print(" _ ");
+				}
+				
+				/*Espacement de la grille*/
+				else{
+					System.out.print("   ");
+				}
+				//System.out.print("["+board[i][j]+"]");
+				
 			}
 			System.out.println();
 		}
-		System.out.println();
+		
+		
 	}
 	
 	public static void displayPiece(int[] piece){
