42e3ae7ea4652c29d1abb144da0e5ef093494358
balastephane <stephane_bala@yahoo.fr>
2016-04-03 23:08:51 +0200

    Malus ajout

--- src/main/java/com/asia/bala_he/GameManager/BoardManager/BoardManager.java
+++ src/main/java/com/asia/bala_he/GameManager/BoardManager/BoardManager.java
@@ -234,8 +234,8 @@ public class BoardManager {
 			}
 			//Si une ligne est remplie
 			if(count == this.board[0].length-6){
-				malusAddRow();
-				//deleteFilledRowAndMovePieces(i);//Supprime la ligne et décale les pièces vers le bas
+				//malusAddRow();
+				deleteFilledRowAndMovePieces(i);//Supprime la ligne et décale les pièces vers le bas
 				//System.out.println("shjgdsjhgds");
 				nbRowDeleted++;
 			}
@@ -305,23 +305,20 @@ public class BoardManager {
 			for (int j = 3; j < this.board[0].length-3; j++) {
 					
 				
-					if(this.board[i][j] !=0  ){
-						if(this.board[i-1][j]==0){
-							this.board[i-1][j] = this.board[i][j];//Décale les pièces vers le haut
-							this.board[i][j] =0;
-						}
-						
+				if(this.board[i][j] !=0  ){
+						this.board[i-1][j] = this.board[i][j];//Décale les pièces vers le haut
+						this.board[i][j] =0;
 					}
-			
+				
 				
 			}
-			
+	
 			
 		}
-		for (int j = 3; j < this.board[0].length-3; j++){
+		for (int k = 3; k < this.board[0].length-3; k++){
 			
-			this.board[this.board.length-4][j] = 8;//Ajoute une ligne
-		
+			this.board[this.board.length-4][k] = 8;//Ajoute une ligne
+	
 		}
 			
 		
